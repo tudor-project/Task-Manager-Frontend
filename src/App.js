3dd2aee355192cab37c21225faa5375b42f9d5e3
Tudor <manoletudor20@gmail.com>
2026-02-03 19:03:34 +0200

    Added logo and changed bg color

--- src/App.js
+++ src/App.js
@@ -16,14 +16,84 @@ function App() {
     const {user, logout} = useContext(AuthContext);
     return (
         <div className="bg-slate-200 flex min-h-screen">
-            <nav className="bg-gray-500 dark:bg-gray-800 w-1/12 h-screen sticky top-0">
-                <ul className="flex flex-col">
-                    <li className="ml-8 m-2 p-2 text-left text-white">
-                        <Link to="/">Home</Link>
+            <nav className="bg-gray-500 dark:bg-[#171a34] w-1/12 h-screen sticky top-0 flex flex-col">
+
+                <div className="mx-2 w-full h-12 flex items-center mb-6">
+                    <span className="text-blue-500 text-2xl font-bold">M</span>
+                    <span className="text-gray-300 font-semibold ml-1">CRM</span>
+                </div>
+
+                <ul className="flex flex-col items-start">
+                    <li className="w-full">
+                        {
+                            !user ? (
+                                <Link
+                                    to="/"
+                                    className="px-2 block text-white text-start py-2 hover:bg-gray-600 transition">
+                                    Home
+                                </Link>
+                            ): (
+                                <Link
+                                    to="/"
+                                    className="px-2 block text-white text-start py-2 hover:bg-gray-600 transition">
+                                    Dashboard
+                                </Link>
+                            )
+                        }
                     </li>
                 </ul>
-                <hr className="my-5 mx-5"/>
+
+                <hr className="my-4 mx-2 border-t border-gray-400/20" />
+
+                <ul className="flex flex-col items-start">
+                    <li className="w-full">
+                        <Link
+                            to="/projects"
+                            className="px-2 block text-white text-start py-2 hover:bg-gray-600 transition">
+                            Task Manager
+                        </Link>
+                    </li>
+                </ul>
+
+                <hr className="my-4 mx-2 border-t border-gray-400/20" />
+
+                <ul className="flex flex-col items-start">
+                    <li className="w-full">
+                        <Link
+                            to="/"
+                            className="px-2 block text-white text-start py-2 hover:bg-gray-600 transition">
+                            CRM
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className="mt-auto px-2 pb-4">
+                    {
+                        !user ? (
+                            <ul className="flex justify-between text-gray-300">
+                                <li>
+                                    <Link to="/login">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/register">Register</Link>
+                                </li>
+                            </ul>
+                        ) : (
+                            <ul className="flex flex-col gap-2 text-gray-300">
+                                <li>
+                                    <span className="font-semibold">{user.name}</span>
+                                </li>
+                                <li
+                                    className="cursor-pointer hover:text-red-400 transition"
+                                    onClick={logout}>
+                                    Logout
+                                </li>
+                            </ul>
+                        )
+                    }
+                </div>
             </nav>
+
 
             <div className="flex-1 flex flex-col">
                 <header className="bg-gray-50 mx-6 my-1 top-0 flex justify-between">
@@ -53,27 +123,7 @@ function App() {
                             </NavLink>
                         </li>
                     </ul>
-                    {
-                        !user ? (
-                            <ul className="flex ">
-                                <li className="m-2 p-2 text-left text-gray-500">
-                                    <Link to="/login">Login</Link>
-                                </li>
-                                <li className="m-2 p-2 text-left text-gray-500">
-                                    <Link to="/register">Register</Link>
-                                </li>
-                            </ul>
-                        ) : (
-                            <ul className="flex">
-                                <li className="m-2 p-2 text-left text-gray-500">
-                                    Salut, <span className="font-semibold">{user.name}</span>
-                                </li>
-                                <li className="m-2 p-2 text-left text-gray-500 cursor-pointer" onClick={logout}>
-                                    Logout
-                                </li>
-                            </ul>
-                        )
-                    }
+
 
                 </header>
 
